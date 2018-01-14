22cd45c08b5a15577e733649c1c1f4ab6fb19ee8
mandy6720 <amanda.hewitt01@gmail.com>
2018-01-14 09:36:27 -0500

    Post build

--- .cache/api-runner-browser.js
+++ .cache/api-runner-browser.js
@@ -1,20 +1,20 @@
 var plugins = [{
-      plugin: require('/Users/amandabozzi/Desktop/amanda-bozzi-site/node_modules/gatsby-remark-autolink-headers/gatsby-browser.js'),
+      plugin: require('/Users/amandabozzi/Desktop/myStuff/amanda-bozzi-coaching/node_modules/gatsby-remark-autolink-headers/gatsby-browser.js'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Users/amandabozzi/Desktop/amanda-bozzi-site/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      plugin: require('/Users/amandabozzi/Desktop/myStuff/amanda-bozzi-coaching/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
       options: {"plugins":[],"trackingId":"UA-110601944-1"},
     },{
-      plugin: require('/Users/amandabozzi/Desktop/amanda-bozzi-site/node_modules/gatsby-plugin-nprogress/gatsby-browser.js'),
+      plugin: require('/Users/amandabozzi/Desktop/myStuff/amanda-bozzi-coaching/node_modules/gatsby-plugin-nprogress/gatsby-browser.js'),
       options: {"plugins":[],"color":"#013243"},
     },{
-      plugin: require('/Users/amandabozzi/Desktop/amanda-bozzi-site/node_modules/gatsby-plugin-catch-links/gatsby-browser.js'),
+      plugin: require('/Users/amandabozzi/Desktop/myStuff/amanda-bozzi-coaching/node_modules/gatsby-plugin-catch-links/gatsby-browser.js'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Users/amandabozzi/Desktop/amanda-bozzi-site/node_modules/gatsby-plugin-twitter/gatsby-browser.js'),
+      plugin: require('/Users/amandabozzi/Desktop/myStuff/amanda-bozzi-coaching/node_modules/gatsby-plugin-twitter/gatsby-browser.js'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Users/amandabozzi/Desktop/amanda-bozzi-site/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      plugin: require('/Users/amandabozzi/Desktop/myStuff/amanda-bozzi-coaching/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
       options: {"plugins":[]},
     }]
 // During bootstrap, we write requires at top of this file which looks
--- .cache/layouts/index.js
+++ .cache/layouts/index.js
@@ -1,7 +1,7 @@
 
   import React from "react"
-  import Component from "/Users/amandabozzi/Desktop/amanda-bozzi-site/src/layouts/index.jsx"
-  import data from "/Users/amandabozzi/Desktop/amanda-bozzi-site/.cache/json/layout-index.json"
+  import Component from "/Users/amandabozzi/Desktop/myStuff/amanda-bozzi-coaching/src/layouts/index.jsx"
+  import data from "/Users/amandabozzi/Desktop/myStuff/amanda-bozzi-coaching/.cache/json/layout-index.json"
 
   export default (props) => <Component {...props} {...data} />
   
